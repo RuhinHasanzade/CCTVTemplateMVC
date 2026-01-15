4dee22acbf988a01ae12e2628e1576af4cf16535
RuhinHasanzade <ruhinhasanzade@gmail.com>
2026-01-15 11:35:28 +0400

    front to back done

---
+++ cctvtemplate/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace cctvtemplate.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
